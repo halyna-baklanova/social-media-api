8bfb8bf76e25459d632bdf0d99cc8434d4406095
halyna-baklanova <novabalka@gmail.com>
2025-01-19 20:31:44 +0200

    implement Email Instead of Username

--- user/serializers.py
+++ user/serializers.py
@@ -6,7 +6,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ("username",)
+        fields = ("username", "email", "password", "is_staff")
         read_only_fields = ("id", "is_staff")
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
