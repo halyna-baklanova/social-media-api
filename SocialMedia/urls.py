0b1b79f68decd50abfa764687069eba6450fe9ec
halyna-baklanova <novabalka@gmail.com>
2025-01-19 20:55:12 +0200

    implement add avatar for profile

--- SocialMedia/urls.py
+++ SocialMedia/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/socialmedia/", include("social_media_api.urls", namespace="socialmedia")),
     path("api/user/", include("user.urls", namespace="user")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- social_media_api/models.py
+++ social_media_api/models.py
@@ -1,16 +1,25 @@
+import pathlib
+import uuid
+
 from django.db import models
 
 from django.utils.timezone import now
-
+from django.utils.text import slugify
 from django.contrib.auth.models import AbstractUser
 
 from user.models import User
+
+def actor_photo_path(instance: "Profile", filename: str) -> pathlib.Path:
+    extension = pathlib.Path(filename).suffix
+    filename = f"{slugify(instance.last_name)}-{uuid.uuid4()}" + extension
+    return pathlib.Path("uploads/avatars/") / pathlib.Path(filename)
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     full_name = models.CharField(max_length=100)
     bio = models.CharField(max_length=255, null=True, blank=True)
+    avatar = models.ImageField(null=True, upload_to="uploads/")
     registration_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
